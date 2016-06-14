92bb426cee7fb5ba3cc4981a0e074ceb5aa5be6d
Victor Lee <lzs_1993@qq.com>
2016-06-14 16:22:34 +0800

    change the launch files for linking with ROS for LabVIEW

--- zs_launch/zs_tools.sh
+++ zs_launch/zs_tools.sh
@@ -1,5 +1,7 @@
 echo "Welcome! zs"
 echo "Let's enjoy with some useful tools!"
+# echo "Setup the ROS_IP as 10.0.126.3"
+# echo "export ROS_IP=10.0.126.3" >> ~/.bashrc
 echo "Sourcing .bashrc file"
 source ~/.bashrc
 echo "Launching MobileSim with the default map"
@@ -7,6 +9,12 @@ echo "Launching MobileSim with the default map"
 echo "Launching rqt_tools"
 rqt_graph &
 rqt_console &
-echo "Launching RViz"
-roslaunch zs_launch_files zs_p3at_rviz.launch
+# zs: MUST RUN roscore HERE; DO NOT RUN MASTER AUTOMATICALLY
+# echo "Launching roscore"
+# roscore &
+# echo "Launching RViz"
+# roslaunch zs_launch_files zs_p3at_rviz.launch
 echo "Finishing!"
+
+# roslaunch zs_launch_files zs_rosaria.launch
+# roslaunch zs_launch_files zs_move_base.launch
